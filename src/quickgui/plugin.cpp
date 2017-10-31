a6ac08b8cdc5a36f6c846e5af2ea0919ecf90c8c
Peter Petrik <zilolv@gmail.com>
2017-10-31 10:34:26 +0100

    start naming convention

--- src/quickgui/plugin.cpp
+++ src/quickgui/plugin.cpp
@@ -3,7 +3,6 @@
 #include "mapimage.h"
 #include "maplayer.h"
 #include "mapview.h"
-#include "positionengine.h"
 #include "project.h"
 #include "qgsnetworkaccessmanager.h"
 
@@ -12,13 +11,6 @@
 #include <qgsmaplayer.h>
 
 #include <qqml.h>
-
-static QObject *_positionEngineProvider(QQmlEngine *engine, QJSEngine *scriptEngine)
-{
-  Q_UNUSED(engine)
-  Q_UNUSED(scriptEngine)
-  return PositionEngine::instance();
-}
 
 static QObject *_projectProvider(QQmlEngine *engine, QJSEngine *scriptEngine)
 {
@@ -37,7 +29,8 @@ void Qgis_Mobile_ComponentsPlugin::registerTypes(const char *uri)
   qmlRegisterType<MapView>(uri, 1, 0, "MapView");
   qmlRegisterType<MapImage>(uri, 1, 0, "MapImage");
   qmlRegisterType<MapLayer>(uri, 1, 0, "MapLayer");
-  qmlRegisterSingletonType<PositionEngine>(uri, 1, 0, "PositionEngine", _positionEngineProvider);
+
   qmlRegisterSingletonType<Project>(uri, 1, 0, "Project", _projectProvider);
+
   qRegisterMetaType< QList<QgsMapLayer*> >( "QList<QgsMapLayer*>" );
 }
