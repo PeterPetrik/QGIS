3a76b103c7b875a1c2578365effa32cf3fad59e4
Martin Dobias <wonder.sk@gmail.com>
2017-12-18 13:27:21 +0100

    Added FeatureModelHighlight and MapTransform QML items
    The highlight is a simplified version of FeatureListModelHighlight from QField,
    MapTransform is a verbatim copy from QField

---
+++ src/quickgui/qgsquickfeaturemodelhighlight.h
@@ -0,0 +1,62 @@
+/***************************************************************************
+              qgsquickfeaturemodelhighlight.h
+               ----------------------------------------------------
+              date                 : 9.12.2014
+              copyright            : (C) 2014 by Matthias Kuhn
+              email                : matthias.kuhn (at) opengis.ch
+ ***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+#ifndef QGSQUICKFEATUREMODELHIGHLIGHT_H
+#define QGSQUICKFEATUREMODELHIGHLIGHT_H
+
+#include <QQuickItem>
+#include "qgis_quick.h"
+
+class QgsQuickMapSettings;
+class QgsQuickFeatureModel;
+
+/**
+ * Creates map highlights for a geometry provided by a FeatureModel.
+ * The highlights are compatible with the QtQuick scene graph.
+ */
+class QUICK_EXPORT QgsQuickFeatureModelHighlight : public QQuickItem
+{
+    Q_OBJECT
+
+    Q_PROPERTY( QgsQuickFeatureModel* model MEMBER mModel NOTIFY modelChanged )
+    Q_PROPERTY( QColor color MEMBER mColor NOTIFY colorChanged )
+    Q_PROPERTY( unsigned int width MEMBER mWidth NOTIFY widthChanged )
+    Q_PROPERTY( QgsQuickMapSettings* mapSettings MEMBER mMapSettings NOTIFY mapSettingsChanged )
+
+  public:
+    explicit QgsQuickFeatureModelHighlight( QQuickItem *parent = 0 );
+
+  signals:
+    void modelChanged();
+    void colorChanged();
+    void mapCanvasChanged();
+    void widthChanged();
+    void mapSettingsChanged();
+
+  private slots:
+    void onDataChanged();
+    void onModelDataChanged();
+
+  private:
+    virtual QSGNode* updatePaintNode( QSGNode *n, UpdatePaintNodeData * ) override;
+
+    QColor mColor;
+    QgsQuickFeatureModel* mModel;
+    bool mDirty;
+    unsigned int mWidth;
+    QgsQuickMapSettings* mMapSettings;
+};
+
+#endif // QGSQUICKFEATUREMODELHIGHLIGHT_H
